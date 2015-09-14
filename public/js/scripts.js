e5a07e73b622950025b4c1d7efa578ed4fb8ecc7
ryan broughan & Andrew Brennwald <student@epicodus.com>
2015-09-14 16:46:09 -0700

    Add styling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,20 +14,34 @@ $(function() {
     var deadline = $("#task-deadline").val();
 
     var task = new Task(name, description, deadline);
+
+    // Create new clickable list item in not-done area
+
     $("#not-done").append("<li><span class='detail'>" + task.name + "</span>" +
                           "<span class='btn btn-info btn-sm complete'>Done</span>" +
+                          "<span class='btn btn-warning btn-sm delete'>Delete</span>" +
                             "<ul class='details'>" +
                               "<li>" + task.description + "</li>" +
                               "<li>" + task.deadline + "</li>" +
                             "</ul></li>");
+
+    // Completes a task when clicked, moving it to the done area, and hides the button
 
     $(".complete").click(function() {
       $("#done").append($(this).parent());
       $(this).hide();
     });
 
+    // Deletes the task
+
+    $(".delete").click(function() {
+      $(this).parent().remove();
+    });
+
+    // Shows and hides task details when clicked
+
     $(".detail").last().click(function() {
-      $(this).next().next().toggle();
+      $(this).nextAll(".details").toggle();
     });
   });
 
